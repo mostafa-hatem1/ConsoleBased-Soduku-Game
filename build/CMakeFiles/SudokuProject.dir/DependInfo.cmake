3df1686806d8b5268e8cfc61b1fb2c7ec5c7f061
mostafa-hatem1 <moustafahatem2005@gmail.com>
2025-06-20 00:38:17 +0300

    Fixed CMake file

---
+++ build/CMakeFiles/SudokuProject.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Hatem/Desktop/Mostafa/Modern C++/C++ Project/Soduku Game/src/SudokuBoard.cpp" "CMakeFiles/SudokuProject.dir/src/SudokuBoard.cpp.obj" "gcc" "CMakeFiles/SudokuProject.dir/src/SudokuBoard.cpp.obj.d"
+  "C:/Users/Hatem/Desktop/Mostafa/Modern C++/C++ Project/Soduku Game/src/SudokuGame.cpp" "CMakeFiles/SudokuProject.dir/src/SudokuGame.cpp.obj" "gcc" "CMakeFiles/SudokuProject.dir/src/SudokuGame.cpp.obj.d"
+  "C:/Users/Hatem/Desktop/Mostafa/Modern C++/C++ Project/Soduku Game/src/SudokuGenerator.cpp" "CMakeFiles/SudokuProject.dir/src/SudokuGenerator.cpp.obj" "gcc" "CMakeFiles/SudokuProject.dir/src/SudokuGenerator.cpp.obj.d"
+  "C:/Users/Hatem/Desktop/Mostafa/Modern C++/C++ Project/Soduku Game/src/SudokuSolver.cpp" "CMakeFiles/SudokuProject.dir/src/SudokuSolver.cpp.obj" "gcc" "CMakeFiles/SudokuProject.dir/src/SudokuSolver.cpp.obj.d"
+  "C:/Users/Hatem/Desktop/Mostafa/Modern C++/C++ Project/Soduku Game/src/main.cpp" "CMakeFiles/SudokuProject.dir/src/main.cpp.obj" "gcc" "CMakeFiles/SudokuProject.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/SudokuProject.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SudokuProject.dir/src/SudokuBoard.cpp.obj"
+  "CMakeFiles/SudokuProject.dir/src/SudokuBoard.cpp.obj.d"
+  "CMakeFiles/SudokuProject.dir/src/SudokuGame.cpp.obj"
+  "CMakeFiles/SudokuProject.dir/src/SudokuGame.cpp.obj.d"
+  "CMakeFiles/SudokuProject.dir/src/SudokuGenerator.cpp.obj"
+  "CMakeFiles/SudokuProject.dir/src/SudokuGenerator.cpp.obj.d"
+  "CMakeFiles/SudokuProject.dir/src/SudokuSolver.cpp.obj"
+  "CMakeFiles/SudokuProject.dir/src/SudokuSolver.cpp.obj.d"
+  "CMakeFiles/SudokuProject.dir/src/main.cpp.obj"
+  "CMakeFiles/SudokuProject.dir/src/main.cpp.obj.d"
+  "SudokuProject.exe"
+  "SudokuProject.exe.manifest"
+  "SudokuProject.pdb"
+  "libSudokuProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SudokuProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
